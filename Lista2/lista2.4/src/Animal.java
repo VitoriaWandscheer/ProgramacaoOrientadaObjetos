de16357facc04345781fe739d2a8d6701ec78d63
Vitória <vitoriawpereira2404@gmail.com>
2023-08-23 17:54:57 -0300

    Create class Animal

---
+++ Lista2/lista2.4/src/Animal.java
@@ -0,0 +1,37 @@
+public class Animal {
+    protected String nome, raca;
+    protected int position;
+
+    public Animal(String nome, String raca) {
+        this.nome = nome;
+        this.raca = raca;
+        this.position = 0;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public int caminha(int passos){
+        this.position = this.position+passos;
+        return this.position;
+    }
+
+    @Override
+    public String toString(){
+        return ("| Nome: "+this.nome+" | Raça: "+this.raca+" | Posição: "+this.position+" |");
+    }
+    
+}
---
+++ Lista2/lista2.4/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        /** Questão 4 */
+        System.out.println("\n---- QUESTÃO 4 ----");
+        System.out.println();
+        Animal an1 = new Animal("Mary Jane", "SRD");
+        System.out.println("Após caminhar "+an1.nome+" está na posição "+an1.caminha(3)+".");
+        System.out.println(an1);
+    }
+}
