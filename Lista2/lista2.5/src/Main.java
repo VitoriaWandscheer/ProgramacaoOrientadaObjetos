4c91d6137055e82eeecd1072a5643966acbfc7c3
Vitória <vitoriawpereira2404@gmail.com>
2023-08-23 20:32:36 -0300

    Create class Pessoa

---
+++ Lista2/lista2.5/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        /** QUESTÃO 5 **/
+        System.out.println();
+        Pessoa p1 = new Pessoa("Raul", 34);
+        System.out.println(p1);
+
+        System.out.println();
+
+    }
+}
---
+++ Lista2/lista2.5/src/Pessoa.java
@@ -0,0 +1,30 @@
+public class Pessoa {
+    protected String nome;
+    protected int idade;
+
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    @Override
+    public String toString(){
+        return "| Nome: "+this.nome+" | Idade: "+this.idade+" |";
+    }
+}
