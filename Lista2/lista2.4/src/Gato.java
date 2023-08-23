ce24f0aff80c719fc0f0265a2e042bfbdf4dea27
Vitória <vitoriawpereira2404@gmail.com>
2023-08-23 20:23:32 -0300

    Create class Gato

---
+++ Lista2/lista2.4/src/Gato.java
@@ -0,0 +1,15 @@
+public class Gato extends Animal {
+    
+    public Gato(String nome, String raca){
+        super(nome, raca);
+    }
+    
+    public String mia(){
+        return "Miau";
+    }
+
+    @Override
+    public String toString(){
+        return ("| GATO | Nome: "+this.nome+" | Raça: "+this.raca+" | Posição: "+this.position+" |");
+    }
+}
