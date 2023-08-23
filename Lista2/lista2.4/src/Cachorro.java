0e0151431cd7f310ac25fef32ece69f17f61fde7
Vitória <vitoriawpereira2404@gmail.com>
2023-08-23 20:23:50 -0300

    Create class Cachorro

---
+++ Lista2/lista2.4/src/Cachorro.java
@@ -0,0 +1,15 @@
+public class Cachorro extends Animal{
+    
+    public Cachorro(String nome, String raca){
+        super(nome, raca);
+    }
+    
+    public String late(){
+        return "Au Au";
+    }
+
+    @Override
+    public String toString(){
+        return ("| CACHORRO | Nome: "+this.nome+" | Raça: "+this.raca+" | Posição: "+this.position+" |");
+    }
+}
