47e332fe4aea90441cecc91e36779df4430cfe3a
Vitória <vitoriawpereira2404@gmail.com>
2023-08-23 21:05:09 -0300

    Create class Miseravel

---
+++ Lista2/lista2.5/src/Miseravel.java
@@ -0,0 +1,11 @@
+public class Miseravel extends Pessoa {
+    
+    public Miseravel(String nome, int idade){
+        super(nome, idade);
+    };
+
+    public String mendiga(){
+        return "Olá senhor Burguês, poderia me dar um pedaço de pão com a grana da mais valia?\nVenho a ti com a voz e o olhar daquele que é marginal e te digo: Enquanto os meios de produção forem propriedade privada o ciclo da exploração será perpetuado.";
+    }
+
+}
