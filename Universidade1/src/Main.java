68b43e63891b5060d321931fb636212cb55b7070
Vitória <vitoriawpereira2404@gmail.com>
2023-09-14 17:59:27 -0300

    Create Universidade

---
+++ Universidade1/src/Main.java
@@ -0,0 +1,13 @@
+package Universidade1;
+/**
+ *
+ * @author Vitória
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        Universidade uni1 = new Universidade("Princeton");
+        System.out.println(uni1);
+        
+    }
+}
---
+++ Universidade1/src/Universidade.java
@@ -0,0 +1,22 @@
+package Universidade1;
+/**
+ *
+ * @author Vitória
+ */
+
+public class Universidade {
+    private String nome;
+
+    public Universidade(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
