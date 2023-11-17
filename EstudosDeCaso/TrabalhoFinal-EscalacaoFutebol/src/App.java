9d302b7fe66a3c40cce413c5d64be668f7636ff7
Vitória <vitoriawpereira2404@gmail.com>
2023-11-17 16:47:56 -0300

    ✨ feat: create App.java

---
+++ EstudosDeCaso/TrabalhoFinal-EscalacaoFutebol/src/App.java
@@ -0,0 +1,73 @@
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("----------------------");
+        System.out.println("--> Trabalho Final <--");
+        System.out.println("----------------------");
+        System.out.println();
+
+
+        // Cria Treinadores
+        Treinador t1 = new Treinador("22222222222", "Rafael", "01/06/1999", "Já treinou diversos times, e até a seleção brasileira.");
+        System.out.println(t1);
+
+        Treinador t2 = new Treinador("44444444444", "Luis", "21/05/1956", "Surfista se aventurando no futebas");
+        System.out.println(t2);
+
+        System.out.println();
+
+
+        //Cria Times
+        Time time1 = new Time(1111, "Esperança", "Estadio da Alegria", t1);
+        System.out.println(time1);
+
+        Time time2 = new Time(2222, "Sol", "Estadio da Luz", t2);
+        System.out.println(time2);
+
+        System.out.println();
+
+
+        // Cria Jogadores
+        Jogador j1 = new Jogador("11111111111", "Vitoria", "24/04/2002", "Goleira", "10",time1);
+        System.out.println(j1.apresentacao());
+
+        Jogador j2 = new Jogador("33333333333", "Dayane", "01/04/2000", "Atacante", "8", time1);
+        System.out.println(j2.apresentacao());
+
+        Jogador j3 = new Jogador("55555555555", "Estevam", "24/04/2002", "Goleiro", "7", time2);
+        System.out.println(j3.apresentacao());
+
+        Jogador j4 = new Jogador("66666666666", "Pedro", "01/04/2000", "Atacante",  "10", time2);
+        System.out.println(j4.apresentacao());
+
+        System.out.println();
+
+
+        // Cria Partida
+        Partida partida1 = new Partida(54321, "17/11/2023 20:00", "Estadio da Alegria");
+        // partida1.addEscalacao(escalacao1);
+        // partida1.addEscalacao(escalacao2);
+        System.out.println(partida1);
+
+        System.out.println();
+
+
+        // Cria Escalação
+        Escalacao escalacao1 = new Escalacao(54321, partida1, t1, time1);
+        escalacao1.addJogador(j1);
+        escalacao1.addJogador(j2);
+        System.out.println(escalacao1);
+
+        Escalacao escalacao2 = new Escalacao(98743, partida1, t2, time2);
+        escalacao2.addJogador(j3);
+        escalacao2.addJogador(j4);
+        System.out.println(escalacao2);
+
+        System.out.println();
+
+
+        // FIM =)
+    }
+
+}
