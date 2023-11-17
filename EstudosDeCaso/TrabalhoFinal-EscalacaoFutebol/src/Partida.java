c03e1cb548592a0a20de18ce4526f495e4b34265
Vitória <vitoriawpereira2404@gmail.com>
2023-11-17 16:50:00 -0300

    ✨ feat: create Partida.java

---
+++ EstudosDeCaso/TrabalhoFinal-EscalacaoFutebol/src/Partida.java
@@ -0,0 +1,11 @@
+public class Partida {
+    int idPartida;
+    String dataHora, local;
+
+    public Partida(int idPartida, String dataHora, String local) {
+        this.idPartida = idPartida;
+        this.dataHora = dataHora;
+        this.local = local;
+    }
+
+}
