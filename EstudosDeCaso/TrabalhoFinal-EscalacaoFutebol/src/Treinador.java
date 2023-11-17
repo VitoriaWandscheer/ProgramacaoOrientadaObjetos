36fe39717d85c57b08eb1f08651beb8a1a013e55
Vitória <vitoriawpereira2404@gmail.com>
2023-11-17 16:49:48 -0300

    ✨ feat: create Treinador.java

---
+++ EstudosDeCaso/TrabalhoFinal-EscalacaoFutebol/src/Treinador.java
@@ -0,0 +1,13 @@
+public class Treinador extends Pessoa{
+    String curriculo;
+
+    public Treinador(String cpf, String nome, String dataNascimento, String curriculo) {
+        super(cpf, nome, dataNascimento);
+        this.curriculo = curriculo;
+    }
+
+    public String apresentacao(){
+        return "Olá, meu nome é " + super.nome + ", nasci em " + super.dataNascimento + " e sou o(a) treinador(a).";
+    }
+
+}
