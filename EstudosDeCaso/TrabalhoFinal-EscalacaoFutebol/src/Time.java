40339660eb7914fee38aa6ddacc4206c00c67457
Vitória <vitoriawpereira2404@gmail.com>
2023-11-17 16:49:16 -0300

    ✨ feat: create Time.java

---
+++ EstudosDeCaso/TrabalhoFinal-EscalacaoFutebol/src/Time.java
@@ -0,0 +1,14 @@
+
+public class Time {
+    int idTime;
+    String nome, estadio;
+    Treinador treinador;
+    
+    public Time(int idTime, String nome, String estadio, Treinador treinador) {
+        this.idTime = idTime;
+        this.nome = nome;
+        this.estadio = estadio;
+        this.treinador = treinador;
+    }
+
+}
