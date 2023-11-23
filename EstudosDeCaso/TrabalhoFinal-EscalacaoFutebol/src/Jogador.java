f175d9ba0c3a04e79d4749ba00e963592d6908de
Vitória <vitoriawpereira2404@gmail.com>
2023-11-23 14:18:59 -0300

    ✨ feat: add getters and setters to Jogador

--- EstudosDeCaso/TrabalhoFinal-EscalacaoFutebol/src/Jogador.java
+++ EstudosDeCaso/TrabalhoFinal-EscalacaoFutebol/src/Jogador.java
@@ -10,8 +10,29 @@ public class Jogador extends Pessoa{
         this.time = time;
     }
 
-    public String apresentacao(){
-        return "Olá, meu nome é " + super.nome + ", nasci em " + super.dataNascimento + " e jogo na posição de "+this.posicao+".";
+    public String getPosicao() {
+        return posicao;
     }
 
+    public void setPosicao(String posicao) {
+        this.posicao = posicao;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public Time getTime() {
+        return time;
+    }
+
+    public void setTime(Time time) {
+        this.time = time;
+    }
+
+    
 }
